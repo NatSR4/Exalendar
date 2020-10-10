b08c82d39eb2366a6e63c685f46bf3de75ac18ac
N-Desmarais <55117352+N-Desmarais@users.noreply.github.com>
2020-10-10 15:14:48 -0400

    Database Connection
    Addign database connection via dotenv node module.
    
    Connect as follows:
    *Create a file called ".env"
    *Add the following fields:
    DB_HOST=*IP*
    DB_USER=*Username*
    DB_PSWD=*Password*

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const result = require("dotenv").config();
+
 const express = require("express");
+var mysql = require("mysql");
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 const views = `${__dirname}/views/`;
+
+// Database Connection
+var connection = mysql.createConnection({
+	host	: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PSWD,
+	database: 'exalendar'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
+
 
 app.get("/", (req,res)=>{
   res.sendFile(views + 'index.html');
