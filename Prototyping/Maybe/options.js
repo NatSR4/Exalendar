be8c847875344aa854ebbcbac09f7ad0d69cd9d0
Joseph <gonzaja5@rpi.edu>
2020-10-19 20:09:48 -0400

    Main fill fixed, button broke, begin to add text

--- Prototyping/Maybe/options.js
+++ Prototyping/Maybe/options.js
@@ -1,6 +1,8 @@
-const sidebar = document.querySelector('.sidebar');
+const sidebar     = document.querySelector('.sidebar');
 const mainContent = document.querySelector('.main-content');
-function sideResponse() {
-  sidebar.classList.toggle('sidebar_small');
-  mainContent.classList.toggle('main-content_large')
+const button      = document.getElementById('sideButton');
+
+button.onclick = function() {
+  sidebar.classList.toggle('sidebar_small'); 
+  mainContent.classList.toggle('main-content_large');
 }
