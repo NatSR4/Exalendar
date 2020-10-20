41c326201203fdf6b9060ba6adf34d097a2aff8d
Joseph <gonzaja5@rpi.edu>
2020-10-19 20:21:40 -0400

    Sidebar collapsable, need 2 fix button position

--- Prototyping/Maybe/options.js
+++ Prototyping/Maybe/options.js
@@ -5,4 +5,12 @@ const button      = document.getElementById('sideButton');
 button.onclick = function() {
   sidebar.classList.toggle('sidebar_small'); 
   mainContent.classList.toggle('main-content_large');
+
+  var div = document.getElementById("sidebarMenu");
+  if (div.style.display !== 'none') {
+    div.style.display = 'none';
+  }
+  else {
+    div.style.display = 'block';
+  }
 }
