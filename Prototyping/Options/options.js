92aa7322fd2a121a222dd142fe45c8a7351e3f2e
Joseph <gonzaja5@rpi.edu>
2020-11-03 16:39:15 -0500

    Buttons staged for Database functions

--- Prototyping/Options/options.js
+++ Prototyping/Options/options.js
@@ -1,6 +1,12 @@
 const sidebar     = document.querySelector('.sidebar');
 const mainContent = document.querySelector('.main-content');
 const button      = document.getElementById('sideButton');
+const resetButton = document.getElementById('submitNewPW');
+const addButton  = document.getElementById('addEntry');
+const delButton   = document.getElementById('removeEntry');
+const exitButton  = document.getElementById('goodbye');
+
+
 
 button.onclick = function() {
   sidebar.classList.toggle('sidebar_small'); 
@@ -12,4 +18,20 @@ button.onclick = function() {
   else {
     div.style.display = 'block';
   }
+}
+
+resetButton.onclick = function() {
+  alert("Button (1) has been pressed");
+}
+
+addButton.onclick = function() {
+  alert("Button (2) has been pressed");
+}
+
+delButton.onclick = function() {
+  alert("Button (3) has been pressed");
+}
+
+exitButton.onclick = function() {
+  alert("Button (4) has been pressed");
 }
