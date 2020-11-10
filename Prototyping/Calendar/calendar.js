78ae1eeacc60fa33a9ae0d77cd4b8164781fcf0d
kySanDSML <71370777+kySanDSML@users.noreply.github.com>
2020-11-09 23:19:30 -0500

    Filled in the calendar js. Having issues getting the previous days to display properly.

--- Prototyping/Calendar/calendar.js
+++ Prototyping/Calendar/calendar.js
@@ -1,1 +1,61 @@
+const date = new Date();
+
+
+const monthDays = document.querySelector('.days')
+
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+const firstDayIndex = date.getDay();
+
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
+
+const nextDays = 7 - lastDayIndex -1;
+
+const months = [
+    "January",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+document.querySelector('.date h2').innerHTML = months[date.getMonth()];
+
+document.querySelector('.date p').innerHTML = date.toDateString();
+
+let days = "";
+//having trouble getting last month's days rendering without breaking the whole calendar.
+//and aligning start of month to correct day of week. 
+for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    monthDays.innerHTML = days;
+  }
+
+for(let i = 1; i <= lastDay; i++){
+    if (
+        i === new Date().getDate() &&
+        date.getMonth() === new Date().getMonth()
+      ) {
+        days += `<div class="today">${i}</div>`;
+      } else{
+        days +=`<div>${i}</div>`;
+      }
+        monthDays.innerHTML = days;
+}
+
+
+for(let j = 1; j <= nextDays; j++){
+    days += `<div class = "next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+
+}
 
