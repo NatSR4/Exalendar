cb159d4b121e4d3889f3e662e6b67067ca081292
Sidharth4032 <sidharth.eage@gmail.com>
2023-02-28 17:38:57 -0500

    Ok this is final

--- app.js
+++ app.js
@@ -39,17 +39,17 @@ app.post('/', urlencodedParser, function (req, res) {
     var event_type = mysql.escape(req.body.etype);
     var event_description = mysql.escape(req.body.edetails);
     var event_date = mysql.escape(req.body.edate);
-var event_time = mysql.escape(req.body.etime);
-var datetime = event_date + ' ' + event_time;
-
-```const sql_code = "INSERT INTO events (class_id, event_type, event_title, event_description, event_date, event_time) 
-VALUES (?, ?, ?, ?, ?, ?)";
-
-connection.query(sql_code, [class_id, event_type, event_title, event_description, event_date, event_time], function (err, results) {
-    if (err) throw err;
-    console.log(results);
-});```
-
+    var event_time = mysql.escape(req.body.etime);
+    var datetime = event_date + ' ' + event_time;
+  
+    const sql_code = `INSERT INTO events (class_id, event_type, event_title, event_description, event_date) 
+      VALUES (?, ?, ?, ?, ?)`;
+  
+    connection.query(sql_code, [class_id, event_type, event_title, event_description, datetime], function (err, results) {
+      if (err) throw err;
+      console.log(results);
+    });
+  
     res.render('main.html', { data: req.body });
 });
 
